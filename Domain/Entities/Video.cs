e04d0c4e92dd7600b27df4b12a6285181348890a
Pierre Meyer <meyer.bbf@gmail.com>
2024-10-06 16:07:59 +0200

    feat: Domain-Entities Video + Playlist erstellt, PlaylistDurationService + PlaylistDurationServiceTests hinzugefügt und Blazor-Projekt integriert
    - Domain-Entities zur Modellierung der Anwendungslogik erstellt
    - PlaylistDurationService implementiert, um die Gesamtdauer der Playlist zu berechnen
    - Testprojekt für die Domain-Schicht hinzugefügt und PlaylistDurationService getestet
    - Blazor-Projekt für die Web-UI hinzugefügt

---
+++ Domain/Entities/Video.cs
@@ -0,0 +1,33 @@
+﻿namespace Domain.Entities
+{
+    public class Video
+    {
+        // Identifikation des Videos
+        public string Id { get; set; }
+
+        // Allgemeine Informationen
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedAt { get; set; }
+
+        // Thumbnail-Informationen
+        public string ThumbnailUrl { get; set; }
+
+        // Kanalinformationen
+        public string ChannelId { get; set; }
+        public string ChannelTitle { get; set; }
+
+        // Kategorie und Lizenz
+        public string CategoryId { get; set; }
+        public string License { get; set; }
+
+        // Sprachinformationen
+        public string DefaultLanguage { get; set; }
+        public string DefaultAudioLanguage { get; set; }
+
+        // Inhaltliche Details
+        public string Duration { get; set; }
+        public bool LicensedContent { get; set; }
+        public string SourceUrl { get; set; }
+    }
+}
---
+++ Domain/PlaylistServices/PlaylistDurationService.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+using Serilog;
+using System.Xml;
+
+namespace Domain.PlaylistServices
+{
+    public class PlaylistDurationService : IPlaylistDurationService
+    {
+        private readonly ILogger _logger;
+        public PlaylistDurationService(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public TimeSpan CalculateTotalDuration(Playlist playlist)
+        {
+            var totalDuration = TimeSpan.Zero;
+
+            foreach (var video in playlist.Videos)
+            {
+                try
+                {
+                    var videoDuration = XmlConvert.ToTimeSpan(video.Duration);
+                    totalDuration += videoDuration;
+                }
+                catch (FormatException ex)
+                {
+                    _logger.Warning("Ungültiges Video-Dauerformat für Video ID: {VideoId}. Dauer: {Duration}. Fehler: {ErrorMessage}",
+                               video.Id, video.Duration, ex.Message);
+                }
+
+            }
+
+            return totalDuration;
+        }
+    }
+}
